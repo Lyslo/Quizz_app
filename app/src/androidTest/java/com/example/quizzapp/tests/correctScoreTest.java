448e85c719a8183884910b716c1cc9e99ee6ca88
Andy-Sandy <andreasstub@hotmail.com>
2023-03-19 17:21:47 +0100

    Completed correctScoreTest 😊

--- app/src/androidTest/java/com/example/quizzapp/tests/correctScoreTest.java
+++ app/src/androidTest/java/com/example/quizzapp/tests/correctScoreTest.java
@@ -1,36 +1,21 @@
 package com.example.quizzapp.tests;
 
-import static androidx.test.InstrumentationRegistry.getTargetContext;
-import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import android.Manifest;
-import android.content.ComponentName;
-
-import androidx.test.espresso.ViewInteraction;
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.intent.Intents;
-import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
-import androidx.test.rule.GrantPermissionRule;
 
 import com.example.quizzapp.MainActivity;
+import com.example.quizzapp.QuizActivity;
 import com.example.quizzapp.R;
-import com.example.quizzapp.ui.newEntry.NewEntryFragment;
+import com.example.quizzapp.model.Item;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-
-import java.util.Random;
 
 @LargeTest
 public class correctScoreTest {
@@ -47,6 +32,21 @@ public class correctScoreTest {
         // Start easy quiz
         onView(withId(R.id.button6))
                 .perform(click());
+
+        // Get correct answer from quiz
+        Item correctItem = QuizActivity.getCurrentItem();
+        String correctAnswer = correctItem.getName();
+
+        // Press the button that has the correct answer
+        onView(withText(correctAnswer))
+                .perform(click());
+
+        // Press the next button
+        onView(withId(R.id.button4))
+                .perform(click());
+
+        // Check that the score has been updated correctly
+        onView(withId(R.id.score)).check(matches(withText("Score: 1 / 1")));
 
     }
 }
