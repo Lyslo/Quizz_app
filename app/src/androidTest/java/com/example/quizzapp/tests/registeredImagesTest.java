a8c1fd974114b334f21f148d59e75b14ef1f659f
h585145 <69844378+h585145@users.noreply.github.com>
2023-04-17 14:21:37 +0200

    Updated test 😎

--- app/src/androidTest/java/com/example/quizzapp/tests/registeredImagesTest.java
+++ app/src/androidTest/java/com/example/quizzapp/tests/registeredImagesTest.java
@@ -1,30 +1,21 @@
 package com.example.quizzapp.tests;
 
-import static androidx.test.InstrumentationRegistry.getTargetContext;
-import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
 
-import android.Manifest;
-import android.content.ComponentName;
+import static org.junit.Assert.assertEquals;
 
 import androidx.lifecycle.LiveData;
-import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
-import androidx.test.rule.GrantPermissionRule;
 
 import com.example.quizzapp.MainActivity;
 import com.example.quizzapp.R;
 import com.example.quizzapp.model.Item;
 import com.example.quizzapp.model.ItemRepository;
-import com.example.quizzapp.ui.newEntry.NewEntryFragment;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -37,9 +28,23 @@ public class registeredImagesTest {
     public ActivityScenarioRule<MainActivity> activityScenarioRule
             = new ActivityScenarioRule<>(MainActivity.class);
 
+    private ItemRepository itemRepository;
     @Test
     public void test_correctNumberOfImages() {
-        onView(withId(R.id.recycler_view))
-                .perform(click());
+        // Get the original length of list
+        LiveData<List<Item>> liveDataItems = itemRepository.getAllItems();
+        int size = liveDataItems.getValue().size();
+        // Delete item at the first position in the recycler view
+        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        // Check if new length after deleting item is correct
+        assertEquals(liveDataItems.getValue().size(), size-1);
+        // Get new original length
+        liveDataItems = itemRepository.getAllItems();
+        int size2 = liveDataItems.getValue().size();
+        // Add item to list
+
+        // Check if new length after adding item is correct
+        assertEquals(liveDataItems.getValue().size(), size2+1);
     }
 }
